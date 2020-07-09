2e098267c51e97c82e05780cded87322c7b18d8d
RuthMaria <ruth-15rms@hotmail.com>
2020-07-09 03:50:54 -0300

    challenge01 finished

--- challenge01/bankingOperations.js
+++ challenge01/bankingOperations.js
@@ -31,35 +31,72 @@ const user = {
 }
 
 function createTransaction(transaction) {
-    user.transactions[0].push(transaction)
+
+    user.transactions.push(transaction)    
 
     if (transaction.type == "credit") {
         user.balance += transaction.value
+    } else {  
+        user.balance -= transaction.value
     }
-
-    user.balance -= transaction.value
 }
 
-function getHigherTransactionByType(TypeTransaction) {
-    const biggerValue = 0, index = 0
 
-    for (const i = 0; i < transactions.length; i++) {
-        if (user[i].transactions.value > biggerValue && user[i].transactions.type == TypeTransaction) {
-            biggerValue = user.transactions.value
+function getHigherTransactionByType(TypeTransaction) {
+    let biggerValue = 0, index = 0
+
+    for (let i = 0; i < user.transactions.length; i++) {
+        if (user.transactions[i].value > biggerValue && user.transactions[i].type == TypeTransaction) {
+            biggerValue = user.transactions[i].value
             index = i
         }
     }
 
-    return user[index]
+    return user.transactions[index]
 }
 
+function getAverageTransactionValue() {
+    let sum = 0, averageTransactions = 0
+    let amount = user.transactions.length
+
+    for (let i = 0; i < user.transactions.length; i++) {
+        sum += user.transactions[i].value
+    }
+    
+    averageTransactions = sum / amount
+
+    return averageTransactions
+}
+
+function getTransactionsCount() {
+    let amountTransactionsOfType = {
+        credit: 0,
+        debit: 0
+    }
+
+    for (let i = 0; i < user.transactions.length; i++) {
+        if (user.transactions[i].type == 'credit') {
+            amountTransactionsOfType.credit++        
+        } else {
+            amountTransactionsOfType.debit++
+        }
+    }
+    
+    return amountTransactionsOfType 
+}
 
 createTransaction({ type: "credit", value: 50 });
 createTransaction({ type: "credit", value: 120 });
 createTransaction({ type: "debit", value: 80 });
 createTransaction({ type: "debit", value: 30 });
 
-console.log("oiiiiiiiiiiii")
-for (const use of user) {
-    console.log(use)
-}
+let biggerValueCredit = getHigherTransactionByType("credit")
+let biggerValueDebit =  getHigherTransactionByType("debit")
+let averageTransactions = getAverageTransactionValue()
+let amountTransactionsOfType = getTransactionsCount()
+
+console.log(user.balance)
+console.log(biggerValueCredit)
+console.log(biggerValueDebit)
+console.log(averageTransactions)
+console.log(amountTransactionsOfType)
