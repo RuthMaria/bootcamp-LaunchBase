654406185b0af5ee2b890d7ae90e82a6f2b026eb
RuthMaria <ruth-15rms@hotmail.com>
2020-09-06 13:11:52 -0300

    challenge04.2: criado a pagina editar e feito o updateda parte admin

--- challenge04.2_foodfy/controllers/admin.js
+++ challenge04.2_foodfy/controllers/admin.js
@@ -10,25 +10,95 @@ exports.create = (req, res) => {
 }
 
 exports.show = (req, res) => {
-    const recipeIndex = req.params.id;
+    const id = req.params.id;
 
-    const recipe = data.recipes[recipeIndex]
+    const recipe = data.recipes[id]
 
-    return res.render('admin/recipe_description', { recipe })
+    return res.render('admin/recipe_description', { recipe, id })
 }
 
 exports.edit = (req, res) => {
-    
+    const id = req.params.id;
+
+    const recipe = data.recipes[id]
+
+    return res.render('admin/edit', { recipe, id })
 }
 
 exports.post = (req, res) => {
-    
+
+    const keys = Object.keys(req.body)
+
+    for (let key of keys) {
+        if (req.body[key] == "") {
+            return res.send("Please, fill all fields!")
+        }
+    }
+
+    let { image, title, author, ingredients, preparation, information } = req.body 
+
+    data.recipes.push( {
+        image, 
+        title, 
+        author, 
+        ingredients, 
+        preparation, 
+        information 
+    })
+
+    fs.writeFile("challenge04.2_foodfy/data.json", JSON.stringify(data, null, 2), err => {
+        if (err) {
+            return res.send("Write file error!")
+        }
+
+        return res.redirect("/admin/recipes")
+    })
 }
 
-exports.put = (req, res) => {
-    
+exports.put = (req, res) => {    
+
+    const keys = Object.keys(req.body)
+
+    for (let key of keys) {
+        if (req.body[key] == "") {
+            return res.send("Please, fill all fields!")
+        }
+    }
+
+    let { image, title, author, ingredients, preparation, information, id } = req.body 
+
+    data.recipes[id] = {
+        image, 
+        title, 
+        author, 
+        ingredients, 
+        preparation, 
+        information 
+    }
+
+    fs.writeFile("challenge04.2_foodfy/data.json", JSON.stringify(data, null, 2), err => {
+        if (err) {
+            return res.send("Write file error!")
+        }
+
+        return res.redirect("/admin/recipes")
+    })
 }
 
 exports.delete = (req, res) => {
+
+    const { id } = req.body
     
+    const position = data.recipes[id]
+
+    const filteredRecipe = data.recipes.filter( recipe => position != id )
+
+    data.recipes = filteredRecipe  
+    
+    fs.writeFile("challenge04/data.json", JSON.stringify(data, null, 2), err => {
+        if (err) {
+            return res.send("Write file error!")
+        }
+        return res.redirect("/recipes")
+    })
 }
--- challenge04.2_foodfy/routes.js
+++ challenge04.2_foodfy/routes.js
@@ -11,9 +11,9 @@ routes.get('/recipes/:index', user.show)
 routes.get("/admin/recipes", admin.index); 
 routes.get("/admin/recipes/create", admin.create); 
 routes.get("/admin/recipes/:id", admin.show); 
-routes.get("/admin/recipes/:id/edit", admin.edit); // Mostrar formulário de edição de receita
+routes.get("/admin/recipes/:id/edit", admin.edit); 
 
-routes.post("/admin/recipes", admin.post); // Cadastrar nova receita
+routes.post("/admin/recipes", admin.post); 
 routes.put("/admin/recipes", admin.put); // Editar uma receita
 routes.delete("/admin/recipes", admin.delete); // Deletar uma receita
 
